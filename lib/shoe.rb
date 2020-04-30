d8a6e656dd3d49863cda1319f8ed8b0f091d9989
Steve Womble <swomble@aol.com>
2020-04-30 20:55:50 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,14 +4,14 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    
+    BRANDS << brand unless 
   end
   
   BRANDS = []
   
   def brand=(brand)
     @brand = brand
-    BRANDS << brand 
+     
   end
 
   def cobble
